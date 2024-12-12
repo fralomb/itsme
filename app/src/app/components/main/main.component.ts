7d00d256104e79ffe7d8379400d05e30accef0a1
Francesco Lombardo <fra.lombardo92@gmail.com>
2024-12-12 12:23:40 +0100

    fixed typewriter service

--- app/src/app/components/main/main.component.ts
+++ app/src/app/components/main/main.component.ts
@@ -1,5 +1,6 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { TypewriterService } from '../../services/typewriter.service';
+import { concat, delay } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -9,22 +10,79 @@ import { TypewriterService } from '../../services/typewriter.service';
   styleUrl: './main.component.scss',
 })
 export class MainComponent implements AfterViewInit {
-  @ViewChild('line1') line1: any;
-  @ViewChild('line2') line2: any;
-  @ViewChild('line3') line3: any;
-  @ViewChild('line4') line4: any;
-  @ViewChild('line5') line5: any;
-  private text_speed: number = 120;
+  @ViewChild('line1') line1: ElementRef | undefined;
+  @ViewChild('line2') line2: ElementRef | undefined;
+  @ViewChild('line3') line3: ElementRef | undefined;
+  @ViewChild('line4') line4: ElementRef | undefined;
+  @ViewChild('line5') line5: ElementRef | undefined;
 
-  constructor(private typewriterService: TypewriterService) {}
+  private text_speed: number = 150;
+  private text_speed_faster: number = 100;
+
+  constructor(private typewriterService: TypewriterService, private renderer: Renderer2) {}
 
   ngAfterViewInit() {
-    console.log(this.line1.nativeElement.innerHTML)
-    var line1_text = this.line1.nativeElement.innerHTML;
-    this.typewriterService.typeText(line1_text, this.text_speed);
-    this.typewriterService.charsTyped.subscribe(text => {
-      this.line1.nativeElement.innerHTML = text;
-    });
+    if (this.line1 && this.line2 && this.line3 && this.line4 && this.line5) {
+      this.renderer.addClass(this.line1.nativeElement, 'typewrite_ongoing');
+
+      concat(
+        this.typewriterService.typeText(this.line1, this.text_speed),
+        this.typewriterService.typeText(this.line2, this.text_speed),
+        this.typewriterService.typeText(this.line3, this.text_speed),
+        this.typewriterService.typeText(this.line4, this.text_speed),
+        this.typewriterService.typeText(this.line5, this.text_speed_faster),
+      )
+      .subscribe({
+        next: (data) => {
+          // console.log(data);
+          switch (data.elem) {
+            case this.line1:
+              this.line1!.nativeElement.innerHTML = data.message;
+              break;
+            case this.line2:
+              if(data.isFirstChar) {
+                this.switchTypewriteClass(this.line1, this.line2)
+              }
+              this.line2!.nativeElement.innerHTML = data.message;
+              break;
+            case this.line3:
+              if(data.isFirstChar) {
+                this.switchTypewriteClass(this.line2, this.line3)
+              }
+              this.line3!.nativeElement.innerHTML = data.message;
+              break;
+            case this.line4:
+              if(data.isFirstChar) {
+                this.switchTypewriteClass(this.line3, this.line4)
+              }
+              this.line4!.nativeElement.innerHTML = data.message;
+              break;
+            case this.line5:
+              if(data.isFirstChar) {
+                this.switchTypewriteClass(this.line4, this.line5)
+              }
+              this.line5!.nativeElement.innerHTML = data.message;
+              break;
+
+            default:
+              break;
+          }
+
+        },
+        error: (error) => {
+          console.error("Error:", error);
+        },
+        complete: () => {
+          // console.log("Observable complete");
+        },
+      });
+    }
+  }
+
+  switchTypewriteClass(elemPrev: ElementRef | undefined, elemNext: ElementRef | undefined) {
+      this.renderer.removeClass(elemPrev!.nativeElement, 'typewrite_ongoing');
+      this.renderer.addClass(elemPrev!.nativeElement, 'typewrite_completed');
+      this.renderer.addClass(elemNext!.nativeElement, 'typewrite_ongoing');
   }
 
 }
